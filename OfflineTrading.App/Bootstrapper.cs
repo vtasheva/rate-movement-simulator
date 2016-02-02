0dd0ebfabb7de8c53bc9538be78057f5a27b36b5
Velina Tasheva <velina_tasheva@yahoo.com>
2016-02-02 14:00:03 +0200

    Added a new project to contain the Shell of the main application

---
+++ OfflineTrading.App/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace OfflineTrading.App
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
