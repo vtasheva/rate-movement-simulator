8c6a18ade9940864bf045444c8351655c9d66dc0
Velina Tasheva <velina_tasheva@yahoo.com>
2016-02-03 14:00:01 +0200

    Added back the projects that will handle the UI interactions

---
+++ OfflineTrading.App/Bootstrapper.cs
@@ -0,0 +1,41 @@
+﻿using Internovus.Wpf.Training.OfflineTrading.App;
+using Microsoft.Practices.Prism.UnityExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Prism.Regions;
+using Internovus.Wpf.Training.OfflineTrading.SelectionModule;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace OfflineTrading.App
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            Container.RegisterType<IRegionManager, RegionManager>();
+
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            var selectionModuleType = typeof(SelectionModule);
+            ModuleCatalog.AddModule(new ModuleInfo() { ModuleName = selectionModuleType.Name, ModuleType = selectionModuleType.AssemblyQualifiedName });
+        }
+    }
+}
---
+++ OfflineTrading.SelectionModule/SelectionModule.cs
@@ -0,0 +1,18 @@
+﻿using Internovus.Wpf.Training.OfflineTrading.SelectionModule.Views;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Modularity;
+
+namespace Internovus.Wpf.Training.OfflineTrading.SelectionModule
+{
+    public class SelectionModule : IModule
+    {
+        public void Initialize()
+        {
+        }
+    }
+}
